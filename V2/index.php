9917441c35533516cb15d629f1d24c86aa77c32b
零起飞 <lx_howlet@163.com>
2019-08-19 11:26:10 +0800

    v2

--- V2/index.php
+++ V2/index.php
@@ -3,8 +3,10 @@ header("Content-type:text/html;charset=utf-8");
 define("ROOT_PATH",str_replace("\\","/",dirname(__FILE__)));
 //判断是否已安装
 if(!is_file("./install/lock") && is_file("./install/index.php")){
+	
 	@header("location:install/index.php");
 	exit;
+	
 }
 define ('APP_NAME', 'ERP' );//也可不定义项目名称，直接将各功能目录与index.php放在一起
 define ('ROOT', dirname(__FILE__));//index.php所在目录
