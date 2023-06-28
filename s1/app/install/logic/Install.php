1a70ee96aa82d665eddef805a674bc0ee2a94f6a
howlet <lx_howlet@163.com>
2023-06-28 10:50:41 +0800

    优化安装脚本程序

--- s1/app/install/logic/Install.php
+++ s1/app/install/logic/Install.php
@@ -2,12 +2,12 @@
 /*
 * install  系统安装
 *
-* =========================================================
-* 零起飞网络 - 专注于网站建设服务和行业系统开发
+* ----------------------------------------------
+* 零起飞网络 - 专注于企业管理系统开发
 * 以质量求生存，以服务谋发展，以信誉创品牌 !
 * ----------------------------------------------
-* @copyright	Copyright (C) 2017-2018 07FLY Network Technology Co,LTD (www.07FLY.com) All rights reserved.
-* @license    For licensing, see LICENSE.html or http://www.07fly.net/html/business
+* @copyright	Copyright (C) 2017-2018 07FLY Network Technology Co,LTD All rights reserved.
+* @license    For licensing, see LICENSE.html
 * @author ：kfrs <goodkfrs@QQ.com> 574249366
 * @version ：1.0
 * @link ：http://www.07fly.xyz
