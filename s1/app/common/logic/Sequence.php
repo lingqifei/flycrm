97b86d8aed78dedeb381eb2c192d0a5095d3f88d
howlet <lx_howlet@163.com>
2023-08-23 15:41:38 +0800

    1、修复自动编号生成重复

--- s1/app/common/logic/Sequence.php
+++ s1/app/common/logic/Sequence.php
@@ -47,7 +47,7 @@ class Sequence extends LogicBase
             $up_data=[
                 'current_value'=>$add_id,
             ];
-            $this->modelSequence->save($up_data,$where);
+            $this->modelSequence->updateInfo($up_data,$where);
         }
 
         $strMaxId=$device_name.$date.$separate.str_pad($add_id,$len,'0',STR_PAD_LEFT );
